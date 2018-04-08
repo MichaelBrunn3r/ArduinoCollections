0d1df81b0f6bd4ce9612e9584b88a766941e885b
Michael Brunner <Michael.Brunn3r@gmail.com>
2018-04-08 23:51:26 +0200

    Moved 'private' up. Method 'resizeTo' now accepts smaller new capacity than current capacity. Added 'const' to some Method parameters. Added Method 'putAll'.

--- Dictionary.h
+++ Dictionary.h
@@ -5,9 +5,33 @@
 
 template <typename T_KEY, typename T_VAL>
 class Dictionary {
+	private:
+		size_t mSize;
+		size_t mCapacity;
+		T_KEY* mKeys;
+		T_VAL* mValues;
+		void resize() {
+			resizeTo(mCapacity * 2);
+		};
+		void resizeTo(int newCapacity) {
+			if(newCapacity != mCapacity) {
+				T_KEY* newKeys = new T_KEY[newCapacity];
+				T_VAL* newValues = new T_VAL[newCapacity];
+
+				memcpy(newKeys, mKeys, newCapacity > mCapacity ? mCapacity * sizeof(T_KEY) : newCapacity * sizeof(T_KEY));
+				memcpy(newValues, mValues,  newCapacity > mCapacity ? mCapacity * sizeof(T_KEY) : newCapacity * sizeof(T_KEY));
+
+				mCapacity = newCapacity;
+				delete[] mKeys;
+				delete[] mValues;
+
+				mKeys = newKeys;
+				mValues = newValues;
+			}
+		}
 	public:
 		Dictionary() : Dictionary(15) {};
-		explicit Dictionary(int capacity) : mCapacity(capacity), mSize(0) {
+		explicit Dictionary(const size_t capacity) : mCapacity(capacity), mSize(0) {
 			T_KEY* keys = new T_KEY[mCapacity];
 			T_VAL* values = new T_VAL[mCapacity];
 
@@ -23,7 +47,7 @@ class Dictionary {
 		bool isFull() {
 			return size() == mCapacity;
 		};
-		bool hasKey(T_KEY key) {
+		bool hasKey(const T_KEY key) {
 			for(int i=0; i<size(); i++) {
 				if(mKeys[i] == key) return true;
 			}
@@ -35,11 +59,9 @@ class Dictionary {
 		T_VAL* values() {
 			return mValues;
 		};
-		void put(T_KEY key, T_VAL val) {
-
+		void put(const T_KEY key, const T_VAL val) {
 			// Override old value if it exists
-			int i=0;
-			for(i; i<size(); i++) {
+			for(int i=0; i<size(); i++) {
 				if(mKeys[i] == key) {
 					mValues[i] = val;
 					return;
@@ -48,37 +70,34 @@ class Dictionary {
 
 			// Add new Key-Value pair
 			if(isFull()) resize();
-			mKeys[i] = key;
-			mValues[i] = val;
+			mKeys[size()] = key;
+			mValues[size()] = val;
 			mSize++;
 		};
-		T_VAL* get(T_KEY key) {
+		void putAll(const int entryc, T_KEY keys[], T_VAL values[], bool fit) {
+			if(fit) {
+				
+			} else {
+				int newCapacity = mCapacity;
+				int overflow = mCapacity - (mSize + entryc);
+				while(overflow > 0) {
+					newCapacity *= 2;
+					overflow = newCapacity - (mSize + entryc);
+				}
+				if(newCapacity > mCapacity) resizeTo(newCapacity);
+			}
+
+			for(int i=0; i<entryc; i++) {
+				put(keys[i], values[i]);
+			}
+		}
+		T_VAL* get(const T_KEY key) {
 			for(int i=0; i<size(); i++) {
 				if(mKeys[i] == key) {
 					return &mValues[i];
 				}
 			}
 			return NULL;
-		};
-	private:
-		size_t mSize;
-		size_t mCapacity;
-		T_KEY* mKeys;
-		T_VAL* mValues;
-		void resize() {
-			size_t newCapacity = mCapacity * 2;
-			T_KEY* newKeys = new T_KEY[newCapacity];
-			T_VAL* newValues = new T_VAL[newCapacity];
-
-			memcpy(newKeys, mKeys, mCapacity * sizeof(T_KEY));
-			memcpy(newValues, mValues, mCapacity * sizeof(T_VAL));
-
-			mCapacity = newCapacity;
-			delete[] mKeys;
-			delete[] mValues;
-
-			mKeys = newKeys;
-			mValues = newValues;
 		};
 };
 
